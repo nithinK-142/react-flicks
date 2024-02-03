d935056940e8ffedce172438801d1e3c7cc4fa7b
nithinK-142 <nithin.sagar.359@gmail.com>
2024-02-03 16:54:15 +0530

    update: button css.

--- src/pages/Bookings.tsx
+++ src/pages/Bookings.tsx
@@ -76,7 +76,7 @@ const Bookings = () => {
               <p>Seats : {booking.seats} </p>
               <Link
                 to={`/shows/${booking.id}`}
-                className="inline-flex items-center w-1/2 px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                className="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 "
               >
                 Book Again!
               </Link>
